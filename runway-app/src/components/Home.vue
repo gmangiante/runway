4dcd4540ee4ca6d801bf108327d524c459162742
Gabriel Mangiante <gabrielmangiante@gmail.com>
2022-11-15 04:10:54 -0800

    home

--- runway-app/src/components/Home.vue
+++ runway-app/src/components/Home.vue
@@ -4,4 +4,30 @@
 
 <template>
   <h3>Welcome to runway!</h3>
+  (where all the models strut their stuff &#x1f31d;)
+  <div class="mt-4"><strong><u>Overview</u></strong></div>
+  <div class="mt-4 mw-50">
+    <strong>runway</strong> is a proof-of-concept project to explore the idea of a highly interactive, mobile-friendly, web-based platform
+    for the construction and sharing of datasets and machine learning models.
+  </div>
+  <div class="mt-4"><strong><u>Architecture</u></strong></div>
+  <div class="mt-4 mw-50">
+    Technologies used include:
+    <ul>
+      <li><a href="https://vuejs.org/" target="_blank" ref="noopener">Vue.js</a> for the front end</li>
+      <li><a href="https://flask.palletsprojects.com/en/2.2.x/" target="_blank" ref="noopener">Flask</a> for the REST API</li>
+      <li><a href="https://www.sqlalchemy.org/" target="_blank" ref="noopener">SQLAlchemy</a> to communicate with the back end database</li>
+      <li><a href="https://www.postgresql.org/" target="_blank" ref="noopener">PostgreSQL</a> for back-end storage</li>
+      <li><a href="https://auth0.com/" target="_blank" ref="noopener">Auth0</a> to handle user management and authentication</li>
+      <li><a href="https://scikit-learn.org/stable/" target="_blank" ref="noopener">scikit-learn</a> for machine learning</li>
+    </ul>
+  </div>
+  <div class="mt-4"><strong><u>Take a look</u></strong></div>
+  <div class="mt-4 mw-50">
+    <ul>
+      <li><RouterLink to="/datasets">Datasets</RouterLink> to view and create datasets</li>
+      <li><RouterLink to="/models">Models</RouterLink> to view and create models</li>
+      <li>NOTE: you must be logged in to create or download datasets and models!</li>
+    </ul>
+  </div>
 </template>
--- runway-app/src/components/model/List.vue
+++ runway-app/src/components/model/List.vue
@@ -10,8 +10,9 @@ import { MDBDatatable, MDBBreadcrumb, MDBBreadcrumbItem } from 'mdb-vue-ui-kit'
 
 const { data } = $(await useFetch<Model[]>('https://runway-demo.herokuapp.com/api/models/'))
 const tableColumns = [
-  { label: 'Sharing', field: 'sharing', width: 50, fixed: true, sort: true },
+  { label: 'Sharing', field: 'sharing', width: 100, fixed: true, sort: true },
   { label: 'Name', field: 'name', sort: true },
+  { label: 'Type', field: 'class_name', sort: true },
   { label: 'Author', field: 'created_by', sort: true },
   { label: 'Train score', field: 'train_score', sort: true},
   { label: 'Val score', field: 'val_score', sort: true}
@@ -36,6 +37,6 @@ const goToDetail = async (tableRowIndex: number) => {
       <MDBBreadcrumbItem active>Models</MDBBreadcrumbItem>
     </MDBBreadcrumb>
   </nav>
-  <MDBDatatable :dataset="tableData" :maxHeight="700" :maxWidth="750" style="cursor:pointer"
+  <MDBDatatable :dataset="tableData" :maxHeight="700" :maxWidth="800" style="cursor:pointer"
         fixedHeader clickable @row-click="goToDetail" :loading="tableLoading" />
 </template>
